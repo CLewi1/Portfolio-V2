18b5e05f07aebdabf63d87037628cd7aeb4f2be2
CLewi1 <75598914+CLewi1@users.noreply.github.com>
2025-09-20 18:46:06 -0500

    feat: add Archive component and implement animated underline for links

---
+++ src/app/archive/page.tsx
@@ -0,0 +1,50 @@
+import Navbar from "@/components/ui/Navbar";
+import Link from "next/link";
+
+interface Projects {
+    year: number;
+    title: string;
+    builtWith: string[];
+    link: string;
+}
+
+export default function Archive() {
+    const projects: Projects[] = [
+        {
+            year: 2023,
+            title: "Personal Portfolio V1",
+            builtWith: ["React", "TailwindCSS", "Vercel"],
+            link: "https://clewi.vercel.app",
+        },
+    ];
+
+
+
+    return (
+        <main className="relative mx-auto mt-6 max-w-xl px-6">
+            <header className="-ml-[8px] mb-10 tracking-tight sm:mb-16">
+                <Navbar />
+            </header>
+
+            <section>
+                
+
+
+
+
+
+
+                {/* Back Home link */}
+                <div className="my-12">
+                    <Link className="group flex items-center gap-1 font-semibold hover:text-neutral-400" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-4 transition-transform duration-300 group-hover:-translate-x-1">
+                        <path d="M6 16L2 12L6 8"></path>
+                        <path d="M2 12H22"></path>
+                        </svg>
+                        <p>Back to home</p>
+                    </Link>
+                </div>
+            </section>
+        </main>
+    );
+}
